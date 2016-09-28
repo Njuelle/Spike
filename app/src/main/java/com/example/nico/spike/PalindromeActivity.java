bc9eee23f14eea9562e073f38339fd314864eb6b
Njuelle <n.juelle@gmail.com>
2016-09-28 14:40:38 +0200

    remove button check + add on change listener on edit text

--- app/src/main/java/com/example/nico/spike/PalindromeActivity.java
+++ app/src/main/java/com/example/nico/spike/PalindromeActivity.java
@@ -6,6 +6,8 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -18,19 +20,30 @@ public class PalindromeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_palindrome);
 
-        Button buttonCheck = (Button) findViewById(R.id.button_check_palindrome);
-        buttonCheck.setOnClickListener(new View.OnClickListener() {
+        final EditText editTextPalindrome = (EditText) findViewById(R.id.edit_text_palindrome);
+        editTextPalindrome.addTextChangedListener(new TextWatcher() {
             @Override
-            public void onClick(View v) {
-                EditText editTextPalindrome = (EditText) findViewById(R.id.edit_text_palindrome);
-                TextView resultPalindrome = (TextView) findViewById(R.id.text_view_result_palindrome);
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                TextView resultPalindrome = (TextView) findViewById(R.id.text_view_result_palindrome);
                 resultPalindrome.setText(checkPalindrome(editTextPalindrome.getText().toString()));
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
             }
         });
     }
 
     private String checkPalindrome(String text) {
+        if (text.isEmpty()) {
+            return "";
+        }
         StringBuffer sb = new StringBuffer(text);
         if (sb.reverse().toString().equals(text)) {
             return "palindrome !";
