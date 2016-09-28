d4cb9f0d2cc2074a82139db86c783f9317a8a309
Njuelle <n.juelle@gmail.com>
2016-09-28 11:07:39 +0200

    Design + get buttons on MainActivity

--- app/src/main/java/com/example/nico/spike/MainActivity.java
+++ app/src/main/java/com/example/nico/spike/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.nico.spike;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button buttonPalindrome = (Button) findViewById(R.id.button_palindrome);
+        Button buttonStringCalculator = (Button) findViewById(R.id.button_string_calculator);
     }
+
 }
